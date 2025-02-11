93baba9acacb50983329596c3f9f0b43086dfccd
eidicaro <icaroeid@gmail.com>
2025-02-11 14:52:19 -0300

    aula1

---
+++ pikachu.php
@@ -0,0 +1,36 @@
+<?php
+    include './header.php';
+?>
+<div class="container">
+    <div class="row d-flex justify-content-content-center">
+        <div class="col-10 p-3">
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="https://i.redd.it/v015pnmeg5m81.gif" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://i.pinimg.com/236x/c1/67/ce/c167ce0bb4362b364dc26044b2288423.jpg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+        </div>
+    </div>
+</div>
+    
+
+
+<?php
+    include './footer.php';
+?>
